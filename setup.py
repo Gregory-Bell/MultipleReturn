cadf037e1e315bf369becb417f8ba11f147e6e8b
Gregory Bell <gregoryscottbell@gmail.com>
2014-12-21 21:37:26 -0800

    More metadata added to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from distutils.core import setup
 
 setup(
     name='MultipleReturn',
-    version='0.1',
-    packages=[''],
-    url='TODO',
-    license='GPL v3',
+    version='0.1.0a1',
+    packages=find_packages(),
+    url='https://github.com/Gregory-Bell/MultipleReturn`',
+    license='GPLv3+',
     author='Gregory Bell',
     author_email='gregoryscottbell@gmail.com',
-    description='An implementation of multiple return.'
+    description='An implementation of multiple return.',
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
 )
