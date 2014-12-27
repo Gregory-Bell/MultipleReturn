fa4f23ac91c883fdbf4e0e4b9146f10cc448adc9
Gregory Bell <gregoryscottbell@gmail.com>
2014-12-27 13:30:06 -0800

    Licenses

--- multiplereturn.py
+++ multiplereturn.py
@@ -1,4 +1,24 @@
+"""
+    Multiple Return
+    Copyright (C) 2014 Gregory S. Bell
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from threading import local
+
+VALUE_ERROR = ValueError("values must be used on the result of a multiple return function")
 
 _local = local()
 
@@ -45,15 +65,13 @@ def values(multiple_return_function_call):
     >>> values(f())
     (1, 2)
     """
-    __all_results_usage_error = ValueError("values must be used on the result of a multiple return function")
-
     try:
         result = _local.long_result
     except AttributeError:
-        raise __all_results_usage_error
+        raise VALUE_ERROR
 
     if multiple_return_function_call != result[0]:
-        raise __all_results_usage_error
+        raise VALUE_ERROR
 
     del _local.long_result
 
--- setup.py
+++ setup.py
@@ -9,11 +9,8 @@ setup(
     author='Gregory Bell',
     author_email='gregoryscottbell@gmail.com',
     description='An implementation of multiple return values.',
-    long_description='Unlike Lisp\'s "values" function, Python lacks a way of creating a function that return multiple '
-                     'values where the caller by default only sees the first one. Simply add the @multiplereturn '
-                     'decorator to any function or method that return a tuple, and it will now return only the first '
-                     'item in that tuple to its callers. Wrap the call to a @multiplereturn decorated function with '
-                     'the values() function and the caller gets the original tuple.',
+    long_description=open("README.md").read(),
+    include_package_data=True,
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Development Status :: 3 - Alpha',
